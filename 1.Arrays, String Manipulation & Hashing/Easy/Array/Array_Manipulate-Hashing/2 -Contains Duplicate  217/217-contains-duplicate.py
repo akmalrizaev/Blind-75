5fcfee7843a75ed23c39632a72bc71b12ebd9fa4
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-01-03 10:35:55 +0500

    Contains Duplicate (LeetCode #217) - Solution

---
+++ 1.Arrays, String Manipulation & Hashing/Easy/Array/Array_Manipulate-Hashing/2 -Contains Duplicate  217/217-contains-duplicate.py
@@ -0,0 +1,11 @@
+def containsDuplicate(nums):
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+
+
+print(containsDuplicate([1, 2, 3, 4]))    # False
+print(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))   # True
